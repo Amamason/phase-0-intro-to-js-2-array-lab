ad724f6fdc529e8976d02358a98d4668dd2464c3
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-02 09:51:13 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfileld');
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+    function appendCat(){
+        var newArray = [...cats, "Broom"];
+        return newArray;
+      }
+      
+      function prependCat(){
+        var newArray = ["Arnold",...cats];
+        return newArray;
+      }
+
+      function removeLastCat(){
+        var newArray = cats.slice(0, cats.length - 1);
+        return newArray;
+      }
+
+      function removeFirstCat(){
+        var newArray = cats.slice(1);
+        return newArray;
+      }
